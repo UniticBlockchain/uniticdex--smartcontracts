1d3acc9ccbdd0a27c46640fdbb1008dbb2bca846
nobita851 <rktipu1995@gmail.com>
2024-03-05 15:13:24 +0530

    MockERC20 contract

---
+++ unitic/contracts/mock/MERC20.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.7.6;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000000000000000000000000);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
